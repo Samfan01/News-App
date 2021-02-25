740a01fcdf37fe649ac4f275c34c96a1f8e93b00
samfan01 <dsamfan@gmail.com>
2021-02-25 12:36:20 +0300

    created the news route to help in displaying the article page

--- app/views.py
+++ app/views.py
@@ -8,4 +8,12 @@ def index():
     View root page function that returns the index
     page and its data
     '''
+    
     return render_template('index.html')
+
+@app.route('/news/<news_url>')
+def news(news_url):
+    '''
+    View news page function that returns news details
+    '''
+    return render_template('news.html'url=news_url)
